71c9d2da5b7c3170155ff1d9c9705c3cb558db0c
Tomas Maria <tomaslbpmaria@hotmail.com>
2023-04-25 13:31:33 +0100

    O JOGO JÁ ACABA! mais ou menos

--- scripts/game.js
+++ scripts/game.js
@@ -9,9 +9,6 @@ class Game {
     this.height = height;
     this.playerChoice = playerChoice;
     this.intervalId = null;
-
-    // Lives
-    this.lives = 2;
   }
 
   createBackground() {
@@ -28,10 +25,50 @@ class Game {
     //ctx.fillText(Score ${this.score}, 80, 30) lives( after)
   }
 
+  gameOn(){
+    document.getElementsByClassName("rock")[0].onclick = () => {
+      player.choice("rock");
+      npc.choice(generatedChoice);
+      this.results(player.option, npc.option);
+      this.winOrLose();
+    };
+    document.getElementsByClassName("paper")[0].onclick = () => {
+      player.choice("paper");
+      npc.choice(generatedChoice);
+      this.results(player.option, npc.option);
+      this.winOrLose();
+    };
+    document.getElementsByClassName("scissors")[0].onclick = () => {
+      player.choice("scissors");
+      npc.choice(generatedChoice);
+      this.results(player.option, npc.option);
+      this.winOrLose();
+    };
+    document.getElementsByClassName("lizard")[0].onclick = () => {
+      player.choice("lizard");
+      npc.choice(generatedChoice);
+      this.results(player.option, npc.option);
+      this.winOrLose();
+    };
+    document.getElementsByClassName("spock")[0].onclick = () => {
+      player.choice("spock");
+      npc.choice(generatedChoice);
+      this.results(player.option, npc.option);
+      this.winOrLose();
+    };
+  }
+
   results(playerChoice, generatedChoice) {
     if (playerChoice === generatedChoice) {
       let result = document.getElementById("round-result");
-      result.innerHTML = `You picked ${playerChoice} and the opponnent picked ${generatedChoice}. The result is a tie!`;
+        result.innerHTML = `You picked ${playerChoice} and the opponnent picked ${generatedChoice}. It's a tie!`;
+        /*ctx.fillStyle = "white";
+        ctx.fillRect(canvas.width / 2 - 175, canvas.height / 2 -100, 400, 250);
+        ctx.font = "20px sans-serif";
+        ctx.fillStyle = "black";
+        ctx.fillText(`You picked ${playerChoice} and `,475, 300);
+        ctx.fillText(`the opponnent picked ${generatedChoice}. `,475, 300);
+        ctx.fillText(`The result is a tie!`,475, 300);*/
     } else if (playerChoice === "rock") {
       if (generatedChoice === "paper") {
         let result = document.getElementById("round-result");
@@ -127,30 +164,51 @@ class Game {
   }
 
   winOrLose() {
+    console.log(player.lives)
+    console.log(npc.lives)
     if (player.lives === 0) {
       console.log("player lost");
       ctx.fillStyle = "white";
-      ctx.fillRect(canvas.width / 2, canvas.height / 2, 400, 250);
-      ctx.font = "16px sans-serif";
+      ctx.fillRect(canvas.width / 2 - 175, canvas.height / 2 -125, 400, 250);
+      ctx.font = "20px sans-serif";
       ctx.fillStyle = "black";
-      ctx.fillText("Bazinga! Better luck next time!", 400, 250);
+      ctx.fillText("Bazinga! Better luck next time!",475, 300);
+
+      setTimeout(() => {
       this.stop();
+      this.clear()
+      player.lives = 2
+      }, 3000);
+      
     } else if (npc.lives === 0) {
       console.log("npc lost");
       ctx.fillStyle = "white";
-      ctx.fillRect(canvas.width / 2, canvas.height / 2, 400, 250);
-      ctx.font = "16px sans-serif";
+      ctx.fillRect(canvas.width / 2 -190, canvas.height / 2 - 125, 400, 250);
+      ctx.font = "20px sans-serif";
       ctx.fillStyle = "black";
-      ctx.fillText("Congratulations! You win!", 400, 250);
+      ctx.fillText("Congratulations! You win!", 500, 300);
+      setTimeout(() => {
       this.stop();
+      this.clear()
+      npc.lives = 2
+      }, 3000);
     }
+  }
+
+  clear() {
+    this.ctx.clearRect(0,0,this.width, this.height);
   }
 
   checkFinishedRound() {} //FALTA FAZER
 
-  stop() {
-    if (player.lives === 0 || npc.lives === 0) {
-      clearInterval(this.intervalId);
-    }
+  start() {
+    this.intervalId = setInterval(this.gameOn, 10);
   }
+
+  stop() {
+    console.log('here')
+      clearInterval(this.intervalId);
+      
+
+    }
 }
--- scripts/index.js
+++ scripts/index.js
@@ -26,36 +26,9 @@ let npc = new Component(
 window.onload = () => {
   document.getElementsByClassName("start-button")[0].onclick = () => {
     let game = new Game(ctx, canvas.width, canvas.height, player);
+    game.clear();
     game.createBackground();
-
-    document.getElementsByClassName("rock")[0].onclick = () => {
-      //debugger; // ativa, chega aqui
-      player.choice("rock");
-      npc.choice(generatedChoice);
-      game.results(player.option, npc.option);
-    };
-    document.getElementsByClassName("paper")[0].onclick = () => {
-      player.choice("paper");
-      npc.choice(generatedChoice);
-      game.results(player.option, npc.option);
-    };
-    document.getElementsByClassName("scissors")[0].onclick = () => {
-      player.choice("scissors");
-      npc.choice(generatedChoice);
-      game.results(player.option, npc.option);
-    };
-    document.getElementsByClassName("lizard")[0].onclick = () => {
-      player.choice("lizard");
-      npc.choice(generatedChoice);
-      game.results(player.option, npc.option);
-    };
-    document.getElementsByClassName("spock")[0].onclick = () => {
-      player.choice("spock");
-      npc.choice(generatedChoice);
-      game.results(player.option, npc.option);
-    };
-    setInterval(() => {
-      game.winOrLose();
-    }, 1000);
+    //game.start();
+    game.gameOn();
   };
 };
